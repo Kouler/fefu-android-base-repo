87b0f45022572bd48ee68686837cd0b89002a412
Kouler <beeproger@yandex.ru>
2021-11-11 00:52:48 +1000

    Created WelcomeActivity, make it work

---
+++ app/src/main/java/ru/fefu/activitytracker/WelcomeActivity.kt
@@ -0,0 +1,10 @@
+package ru.fefu.activitytracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity(R.layout.welcome_activity) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
